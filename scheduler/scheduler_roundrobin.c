3686d0d88a665fa85d3bdd3abbea116e6daa514a
LUCAS MENEZES OLIVEIRA SOUSA <c1310844@niger.grad.inf.puc-rio.br>
2014-09-25 12:37:05 -0300

    scheduler & shell modificado

---
+++ scheduler/scheduler_roundrobin.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#define EVER ;;
+int main (int argc, char** argv)
+{
+    int pid1;
+    int pid2;
+    
+    //int i;
+    if(argc!=1)
+    {
+        printf("Esse programa executa apenas os programas pre-configurados\n");
+        return 1;
+    }
+    printf( "Parent %d\n",getpid());
+    pid1 = fork();
+    pid2=  fork();
+    printf( "1 %d 2 %d\n",pid1,pid2);
+       if (pid1 == 0 && pid2!=0)                // child
+       {
+                      
+                 printf("pid1 %d\n",getpid());
+                execl("lab4.2.a",NULL);
+            
+         
+        }
+        else if (pid1 < 0)            // failed to fork
+        {
+            printf("Fork não deu certo!\n");
+            exit(1);
+            // Throw exception
+        }
+                                       // parent
+        if (pid2 == 0 && pid1!=0)                // child
+       {
+                      
+                 
+                printf("pid2 %d\n",getpid());
+                execl("lab4.2.b",NULL);
+         
+        }
+        else if (pid2 < 0)            // failed to fork
+        {
+            printf("Fork não deu certo!\n");
+            exit(1);
+            // Throw exception
+        }
+        else if(pid1!=0 && pid2!=0)
+        {    printf("Parent pid %d\n",getpid());
+        //kill(pid1, SIGINT);
+        //kill(pid2, SIGINT);
+        for(EVER)
+        {
+        kill(pid1, SIGINT);
+        kill(pid2, SIGCONT);
+            fflush(stdout);
+            printf("Go %d pid2\n",pid2);
+        sleep(3);
+        kill(pid1, SIGCONT);
+        kill(pid2, SIGINT);
+            fflush(stdout);
+            printf("Go %d pid1\n",pid1);
+        sleep(3);
+        }
+        }
--- shell.c
+++ shell.c
@@ -1,9 +1,23 @@
 
 #include<stdio.h>
 
-int main (void)
+int main (int argc, char** argv)
 {
+int i;
+if(argc<2)
+{
+printf ("Shell espera parametros: \n\n");
+printf("exec <nome_programa> prioridade=<numero de 1 a 7> , no caso de lista de prioridades\n");
 
+printf("exec <nome_programa> tempoexec=<tempo em ms>, no caso de SJF\n");
+
+printf("exec <nome_programa>, no caso de ROUND-ROBIN\n"); 
+return 1;
+}
+for(i=1;i<argc;i++)
+{
+	printf("%s\n",argv[i]);
+}
 
 return 0;
 }
