70b0544fda41b51be090c27015df0166b5dcdb4b
root <root@localhost.localdomain>
2014-10-06 22:10:36 -0300

    Primeiro Tratamento da Shell

--- shell.c
+++ shell.c
@@ -1,23 +1,53 @@
 
 #include<stdio.h>
-
+#include<string.h>
 int main (int argc, char** argv)
 {
-int i;
-if(argc<2)
-{
-printf ("Shell espera parametros: \n\n");
-printf("exec <nome_programa> prioridade=<numero de 1 a 7> , no caso de lista de prioridades\n");
+	int i;
+	if(argc<3) // 1 shell 2 exec 3 <nome_programa>
+	{
+		printf ("Shell espera parametros: \n\n");
+		printf("exec <nome_programa> prioridade=<numero de 1 a 7> , no caso de lista de prioridades\n");
 
-printf("exec <nome_programa> tempoexec=<tempo em ms>, no caso de SJF\n");
+		printf("exec <nome_programa> tempoexec=<tempo em ms>, no caso de SJF\n");
 
-printf("exec <nome_programa>, no caso de ROUND-ROBIN\n"); 
-return 1;
-}
-for(i=1;i<argc;i++)
-{
-	printf("%s\n",argv[i]);
-}
+		printf("exec <nome_programa>, no caso de ROUND-ROBIN\n"); 
+		return 1;
+	}
 
-return 0;
+	for(i=1;i<argc;i++)
+	{	
+		printf("%s\n",argv[i]);	
+	}
+		if(strcmp(argv[1],"exec")==0)
+		{	
+			if(argc==3)
+			{ /* Call Round Robin */
+			printf("Calling RR\n");
+			return 0;
+			}
+			if(strncmp("tempoexec=",argv[3],10)==0);
+			{
+				/*Call SJF */
+			printf("Calling SJF\n");
+				return 0;
+			}
+			if(strncmp("prioridade=",argv[3],11)==0)
+			{
+			printf("Calling priority Lists\n");
+				/* Call Priority Lists */
+				return 0;
+			}
+			else
+			{
+			printf("Tente: ./shell exec <nome do programa> | prioridade=<numero de 1 a 7> | tempoexec=<tempo em ms> \n");
+			return 2;
+			}
+		}
+		
+		
+	
+	
+
+	return 0;
 }
