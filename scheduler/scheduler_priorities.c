5135740b51b98c4f9a5884f9f6e5be0f596bf667
Lucas <great-lucas@hotmail.com>
2014-10-10 18:28:46 -0300

    prioridades supostamente funcionando

--- scheduler/scheduler_priorities.c
+++ scheduler/scheduler_priorities.c
@@ -6,10 +6,10 @@
 #define CPUTIME 50  //Mileseconds
 #define ON 1
 #define OFF 0
-#define DEBUGMSGS ON
+#define DEBUGMSGS OFF
 static void filhoHandler (int sinal);
 static void alarmHandler (int sinal);
-
+static int getMaximum(int* pri, int tam);
 
 static int pos,procFim=0;// posiçao do processo corrente e o numero dos processo finalizados
 static int *pids;
@@ -52,6 +52,11 @@ int scheduler_PL (int* priorities, char **path, int tam)
     int i,j;// contadores
 	int bol1;
 	int result;
+	int currentPri;
+	currentPri=getMaximum(priorities,tam);
+	if(currentPri<-1)
+	{	printf("PL: Priority List Currupted\n");
+	}
 	
  		pids = (int*) malloc(tam*sizeof(int));
 		t_ini = (int*) malloc(tam*sizeof(int));
@@ -74,7 +79,8 @@ int scheduler_PL (int* priorities, char **path, int tam)
 		if(pids[i] == 0)// filho
 		{	
 			if(DEBUGMSGS)printf ("entre o execv e o kill do processo %d\n",i);
-			execl(path[i],path[i],NULL);
+			if(execl(path[i],path[i],NULL)==-1)
+			printf("PL: Coudn't alloc Process\n");
 			
 			//kill(pids[i],SIGSTOP);
 			//kill(pids[i],SIGSTOP);
@@ -99,8 +105,10 @@ int scheduler_PL (int* priorities, char **path, int tam)
 			if (pids[pos] == -1) //Se o programa ja acabou, pulamos ele
 				continue;	
 			t_ini[pos] = (int)time(NULL);
-			kill(pids[pos],SIGCONT);
-			usleep(cpuTime);			
+			if(priorities[pos]==currentPri) //Somente se for a prioridade da vez
+			{	kill(pids[pos],SIGCONT); //Se não for não continue
+				usleep(cpuTime);
+			}			
 			result = waitpid(pids[pos],&status,WNOHANG);
 			if(DEBUGMSGS)printf ("result e status %d %d\n",result,status);
 			if(result!=0)
@@ -108,14 +116,22 @@ int scheduler_PL (int* priorities, char **path, int tam)
 				printf ("processo %d terminado\n",pos);
 				procFim++;
 				pids[pos]=-1;
-			}			
-			ualarm(cpuTime,0);
+			}
+						
+			if(priorities[pos]==currentPri)
+			{	ualarm(cpuTime,0);
+				if(priorities[pos]>1)
+					priorities[pos]--; //prioridade cai
+				currentPri=getMaximum(priorities,tam); //Atualize a prioridade corrente			
+				
 				/* equals to alarm in microseconds */			
 			//kill(pids[pos],SIGSTOP);		
-			
+			}
 			if(DEBUGMSGS)printf ("dentro do loop RR - %d\n",pos);
 			//t_fim[pos] = (int)time(NULL);
 			turnaround[pos] += cpuTime;
+			//printf("currentPri=%d\n",currentPri);
+			//printf("pri %d that pri= %d\n",i,priorities[i]);
 			
 		}
 	}
@@ -125,4 +141,16 @@ int scheduler_PL (int* priorities, char **path, int tam)
    		
 		printf ("turn around do processo %d é : %.3f segundos\n",pos,turnaround[pos]/1000000.0);
 	}
+}
+static int getMaximum(int* pri,int tam)
+{
+  int x=-99;
+  int i;
+	for(i=0;i<tam;i++)
+	{
+		if(x<pri[i])	
+			x=pri[i];
+	}
+	return x;
+
 }
