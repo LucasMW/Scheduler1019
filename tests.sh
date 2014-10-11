52c1c246379dc4bbab7948b6b4fae424b3a4e799
root <great-lucas@hotmail.com>
2014-10-10 21:40:24 -0300

    Aproaching release and better instructions

--- tests.sh
+++ tests.sh
@@ -1,4 +1,8 @@
 echo -e "running tests"
 ./shell -interpreter >logRR.txt
+echo -e "1/3"
 ./shell -interpreter entrada3.txt >logSJF.txt
-./shell -interpreter entrada4.txt>logPL.txt
+echo -e "2/3"
+./shell -interpreter entrada2.txt>logPL.txt
+echo -e "3/3"
+echo -e "Procure por logRR.txt, logSJF.txt e logPL.txt"
